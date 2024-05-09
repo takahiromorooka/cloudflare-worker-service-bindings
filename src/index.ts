896d7ae0b05f9dd8746d21c23218d21feee68d9e
takahiromorooka <t.morooka32@gmail.com>
2024-05-09 15:02:40 +0900

    tmp

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,41 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { WorkerEntrypoint } from "cloudflare:workers";
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+// NOTE: Overhead 検証に使った
+// function sleep(time: number) {
+//   return new Promise(resolve => setTimeout(resolve,time));
+// }
+
+// See: https://developers.cloudflare.com/workers/runtime-apis/bindings/service-bindings/rpc/
+// export default class TestRpcService extends WorkerEntrypoint {
+//   async getSomething() {
+// 		return new Response("RPC desu!!!");
+//   }
+// };
+
+export default class extends WorkerEntrypoint {
+  // See: https://developers.cloudflare.com/workers/runtime-apis/bindings/service-bindings/rpc/#lifecycle-methods-ctx
+  async #sendEvent() {
+    try {
+      const res =  await fetch("https://www.google.co.jp/", { method: "GET" });
+      const json = await res.text();
+      console.log(json);
+      return res;
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async fetch(request: Request) {
+    try {
+      // 呼び出し元のworkerが終了しても、こちらの処理は実行用にする記述
+      this.ctx.waitUntil(this.#sendEvent())
+      const url = request.url;
+      console.log(url)
+      return new Response(); 
+    } catch(e) {
+      console.log(e);
+      return new Response();
+    }
+  }
+}
+
